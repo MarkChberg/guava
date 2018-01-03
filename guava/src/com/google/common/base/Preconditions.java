0a8c432a4d8285aad4c0e0fc8e4fb15b707b8364
wangjiatao <wangjiatao@jd.com>
2018-01-03 17:52:07 +0800

    learning

--- guava/src/com/google/common/base/Preconditions.java
+++ guava/src/com/google/common/base/Preconditions.java
@@ -880,7 +880,7 @@ public final class Preconditions {
   public static <T> T checkNotNull(T reference) {
     if (reference == null) {
       throw new NullPointerException();
-    }
+  }
     return reference;
   }
 
